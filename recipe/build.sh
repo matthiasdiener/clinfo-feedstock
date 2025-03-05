910e49d55ae5c27321b80c934acd3a3960083850
Matthias Diener <mdiener@illinois.edu>
2025-03-05 14:27:55 -0600

    work around build failure on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+set -ex
+
+if [[ $target_platform == osx-* ]]; then
+    # Allow overriding conflicting types in the khronos headers
+    rm $PREFIX/include/CL/cl_ext.h
+    touch $PREFIX/include/CL/cl_ext.h
+fi
+
 make install OS=Linux PREFIX=$PREFIX
